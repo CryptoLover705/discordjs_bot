80ae500b427cb614244f09a6a9ceaece7f1f6c88
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2020-07-25 10:49:19 +0100

    added a sample ping command & bot activity status

--- index.js
+++ index.js
@@ -16,4 +16,16 @@ client.once('ready', () => {
     console.log(`${client.user.username} is now online`) // This is just some logging so it will tell you when the bot is online
 });
 
+// Setting of the bot's activity status
+client.on('ready', () => {
+    client.user.setActivity("CryptoLoverBots", { type: "STREAMING", url: 'https://cryptoloverbots.com/index.html'})
+});
+
+client.on('message', message => {
+    // If the message is "ping", The bot will reply "pong"
+    if(message.content == "ping") {
+        return message.channel.send('pong 🏓')
+    }
+});
+
 client.login(process.env.TOKEN) // this allows us to use a .env 
